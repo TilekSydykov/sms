63895ad2bcab3d1b5ff3a8060a6ca0743294b7ef
Tilek Sydykov <tilekfromkg@gmail.com>
2022-01-05 18:10:31 +0600

    deploy

--- controllers/main-controller.go
+++ controllers/main-controller.go
@@ -106,6 +106,7 @@ func (m *MainController) MainForm(c *gin.Context) {
 			Data: gin.H{
 				"number": c.PostForm("number"),
 				"text":   c.PostForm("text"),
+				"id":     123,
 			},
 		})
 		if err != nil {
